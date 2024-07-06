4e6a2033b74698cdf1072fb5d75f8ba09b24a03e
Rohan265 <61971470+Rohan265@users.noreply.github.com>
2024-07-06 21:06:15 +0530

    Add files via upload

---
+++ src/test/java/com/rohan/entity_relations/EntityRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.entity_relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
